bf5a421d798583ed58a67dba3e69f5dc1e5f3ec3
lixin <2237498407@qq.com>
2023-01-04 20:43:24 +0800

    feat: 添加自定义异常的基本定义

---
+++ src/main/java/indl/lixn/yimm/common/exception/CustomException.java
@@ -0,0 +1,14 @@
+package indl.lixn.yimm.common.exception;
+
+/**
+ * @author lixn
+ * @description
+ * @date 2023/01/04 20:41
+ **/
+public class CustomException extends RuntimeException {
+
+    private String errorMessage;
+
+
+
+}
---
+++ src/main/java/indl/lixn/yimm/common/exception/ExceptionNodeEnum.java
@@ -0,0 +1,12 @@
+package indl.lixn.yimm.common.exception;
+
+/**
+ * @author lixn
+ * @description
+ * @date 2023/01/04 20:42
+ **/
+public enum ExceptionNodeEnum {
+
+
+
+}
